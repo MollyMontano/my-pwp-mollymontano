c9d0e43f197970aa13c6eb7f93c780da92b07532
Molly Montano <MMontano167@cnm>
2020-11-15 09:23:51 -0700

    recaptcha bug fix hopefully

--- public_html/script/script.js
+++ public_html/script/script.js
@@ -46,7 +46,8 @@ $(document).ready(function () {
         errorLabelContainer: '#output-area',
         errorElement:"div",
 
-
+        // rules here define what is good or bad input
+        //each rule starts with the form input element's NAME attribute
         rules: {
             name: {
                 required: true,
@@ -70,10 +71,9 @@ $(document).ready(function () {
             },
             message: {
                 required: "Message is a required field",
-                maxlength: "message is too long"
+                maxlength: "message is to long"
             }
         },
-
         submitHandler: (form) => {
             $("#contact").ajaxSubmit({
                 type: "POST",
@@ -91,6 +91,64 @@ $(document).ready(function () {
     })
 })
 
-
-
-
+// $(document).ready(function () {
+//     $("#contact").validate({
+//         debug: true,
+//         errorClass: "alert alert-danger",
+//         errorLabelContainer: '#output-area',
+//         errorElement:"div",
+//
+//
+//         rules: {
+//             name: {
+//                 required: true,
+//             },
+//             email: {
+//                 email: true,
+//                 required: true
+//             },
+//             message:{
+//                 required: true,
+//                 maxlength: 2000
+//             }
+//         },
+//         messages: {
+//             name:{
+//                 required: "Name is a required field",
+//             },
+//             email: {
+//                 required: "is a required field",
+//                 email: "please provide a valid email"
+//             },
+//             message: {
+//                 required: "Message is a required field",
+//                 maxlength: "message is too long"
+//             }
+//         },
+//
+//         submitHandler: (form) => {
+//             $("#contact").ajaxSubmit({
+//                 type: "POST",
+//                 url: $("#contact").attr('action'),
+//                 success: (ajaxOutput) => {
+//                     $("#output-area").css("display", "")
+//                     $("#output-area").html(ajaxOutput)
+//
+//                     if($(".alert-success").length >= 1) {
+//                         $("#contact")[0].reset()
+//                     }
+//                 }
+//             })
+//         }
+//     })
+// })
+//
+//
+// $("#contact").ajaxSubmit({
+//     type: "POST",
+//     url: $("#contact").attr('action'),
+//     success: () => {
+//         success: (ajaxOutput) => {
+//             $("#output-area").css("display", "")
+//             $("#output-area").html(ajaxOutput)
+//
