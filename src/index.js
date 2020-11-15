e1a02cfe143e5eb4bb72836ea2a459f3111288fb
Molly Montano <MMontano167@cnm>
2020-11-14 17:13:56 -0700

    added back end for contact me form

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ const express = require("express")
 const morgan = require("morgan");
 const {check, validationResult} = require("express-validator")
 const app = express();
-// const bodyParser = require("body-parser")
-// const mailgun = require("mailgun-js")
-// const Recaptcha = require("express-recaptcha").RecaptchaV2
+const bodyParser = require("body-parser")
+require('dotenv').config()
+const mailgun = require("mailgun-js")
+const Recaptcha = require("express-recaptcha").RecaptchaV2
 
 
 
@@ -22,83 +23,60 @@ app.use(express.json());
 const indexRoute = express.Router();
 
 
+const recaptcha = new Recaptcha(process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SECRET_KEY)
 
 
+const validation = [
+    check("email", "please provide a valid email").isEmail(),
+    check("name", "A valid name is required").not().isEmpty().trim().escape(),
+    check("subject").optional().trim().escape(),
+    check("message", "A message shorter than 2000 characters is required").not().isEmpty().trim().escape().isLength({max:2000})
+]
+
+if(errors.isEmpty()===false) {
+    const currentError = errors.array()[0]
+
+    return response.send(`<div class="alert alert-danger" role='alert'><strong>Oh snap!
+        </strong>There was an error with Recaptcha please try again</div>`)
+}
+
+if(request.recaptcha.error) {
+    return response.send(`<div class='alert alert-danger' role='alert'>`)
+    }
+
+const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN})
+
+ const {email, subject, name, message} = request.body
 
 
-// const recaptcha = new Recaptcha(process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SECRET_KEY)
-//
-//
-// const validation = [
-//     check("email", "please provide a valid email").isEmail(),
-//     check("name", "A valid name is required").not().isEmpty().trim().escape(),
-//     check("subject").optional().trim().escape(),
-//     check("message", "A message shorter than 2000 characters is required").not().isEmpty().trim().escape().isLength({max:2000})
-// ]
-//
-//
-// const handleSendingEmail = (request,response, next) => {
-//     response.append("Content-Type","text/html")
-//     const errors = validationResult(request)
-//
-//
-//     if(errors.isEmpty()===false) {
-//         const currentError = errors.array()[0]
-//
-//
-//         return response.send(`<div class="alert alert-danger" role="alert"><strong>Oh snap!</strong> ${currentError.msg}</div>`)
-//     }
-//
-//
-//     if(request.recaptcha.error) {
-//         return response.send(`<div class='alert alert-danger' role='alert'><strong>Oh snap!</strong>There was an error with Recaptcha please try again</div>`)
-//     }
-//
-//
-//     const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN})
-//
-//
-//     const {email, subject, name, message} = request.body
-//
-//
-//     const mailgunData = {
-//         to: process.env.MAIL_RECIPENT,
-//         from: `Mailgun Sandbox <postmaster@${process.env.MAILGUN_DOMAIN}>`,
-//         subject: `${name} - ${email} : ${subject}`,
-//         text: message
-//     }
-//
-//
-//     mg.messages().send(mailgunData, (error) => {
-//         if (error) {
-//             return response.send(Buffer.from(`<div class='alert alert-danger' role='alert'><strong>Oh snap!</strong> Unable to send email error with email sender</div>`))
-//         }
-//         return response.send(Buffer.from("<div class='alert alert-success' role='alert'>Email successfully sent.</div>"))
-//     })
-//
-//
-//
-//
-// }
-//
-//
-// const handleGetRequest = (request, response) => {
-//     return response.json("The express server is live");
-// }
+const handleSendingEmail = (request, response) => {
+    response.append("Content-Type","text/html")
+    const errors = validationResult(request)
+}
+    
+ const mailgunData = {
+        to: process.env.MAIL_RECIPENT,
+        from: `Mailgun Sandbox <postmaster@${process.env.MAILGUN_DOMAIN}>`,
+        subject: `${name} - ${email} : ${subject}`,
+        text: message
+    }
+
+
+const handleGetRequest = (request, response) => {
+    return response.json("The express server is live");
+}
 
 
 // Example express configuration for our /apis/ route.
 indexRoute.route("/test")
-    .get(function (request, response,next) {
-     console.log("request", request)
-        next()
-    },function (request, response,next){
-        return response.json({status:200, message: "Is this thing?"});
-    })
+    .get(handleGetRequest)
+    .post(recaptcha.middleware.verify, handleSendingEmail)
+
+
     // .post(recaptcha.middleware.verify ,validation, handleSendingEmail)
 
 
-app.use("/express", indexRoute);
+app.use("/apis", indexRoute);
 
 
 //app.listen declares what port the Express application is running on
