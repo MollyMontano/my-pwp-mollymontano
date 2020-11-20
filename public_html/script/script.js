f8bd1c5a5f961c89b91c105ec7d0f853b1e97931
Molly Montano <MMontano167@cnm>
2020-11-20 04:46:57 -0700

    working on spacing

--- public_html/script/script.js
+++ public_html/script/script.js
@@ -1,7 +1,7 @@
-// // MDB Lightbox Init
-// $(function () {
-//     $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
-// });
+
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+})
 
 function openModal() {
     document.getElementById("mySlides").style.display = "block";
@@ -10,34 +10,4 @@ function openModal() {
 function closeModal() {
     document.getElementById("mySlides").style.display = "none";
 }
-
-// let slideIndex = 1;
-// showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-// function showSlides(n) {
-//     let i;
-//     let slides = document.getElementsByClassName("mySlides");
-//     let dots = document.getElementsByClassName("demo");
-//     let captionText = document.getElementById("caption");
-//     if (n > slides.length) {slideIndex = 1}
-//     if (n < 1) {slideIndex = slides.length}
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
-//     }
-//     for (i = 0; i < dots.length; i++) {
-//         dots[i].mySlides= dots[i].mySlides.replace(" active", "");
-//     }
-//     slides[slideIndex-1].style.display = "block";
-//     dots[slideIndex-1].mySlides += " active";
-//     captionText.innerHTML = dots[slideIndex-1].alt;
-// }
-
 
